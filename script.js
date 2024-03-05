97840db67f7c34a582b2267d861c43f5dee88200
Andrea Visani <visaniandrea.92@gmail.com>
2024-03-05 12:08:28 -0500

    Added JS functionality for projects

--- script.js
+++ script.js
@@ -22,3 +22,47 @@ function toggleNavbar() {
     var navbarContainer = document.querySelector('.bottom-navbar-container');
     navbarContainer.classList.toggle('show-navbar');
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const cardContainer = document.querySelector('.project-container');
+    const cards = cardContainer.children; // Select direct children of .project-container
+    let currentCardIndex = 0;
+
+    function showCard(index) {
+        for (let i = 0; i < cards.length; i++) {
+            cards[i].style.display = i === index ? 'block' : 'none';
+        }
+    }
+
+    function showNextCard() {
+        if (currentCardIndex === cards.length - 1) {
+            currentCardIndex = 0;
+        } else {
+            currentCardIndex++;
+        }
+        showCard(currentCardIndex);
+    }
+
+    function showPreviousCard() {
+        currentCardIndex = (currentCardIndex - 1 + cards.length) % cards.length;
+        showCard(currentCardIndex);
+    }
+
+    document.querySelectorAll('.card-button.next').forEach(function (button) {
+        button.addEventListener('click', showNextCard);
+    });
+
+    document.querySelectorAll('.card-button.previous').forEach(function (button) {
+        button.addEventListener('click', showPreviousCard);
+    });
+
+    // Show the first card initially
+    showCard(currentCardIndex);
+});
+
+
+
+  
+
+
